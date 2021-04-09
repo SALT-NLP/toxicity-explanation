e0b98c8b92cf21328609c3aa5d430f6e44b555a0
rsridhar37 <Dude@Rohits-MBP.attlocal.net>
2021-04-08 20:49:51 -0700

    Added seq2seq model code and utils

--- src/seq2seq.py
+++ src/seq2seq.py
@@ -1,94 +1,194 @@
 from torch import nn, torch
-from transformers import BertTokenizer, BertForSequenceClassification
-from transformers import BartTokenizer, BartForConditionalGeneration
+from transformers import BertForSequenceClassification
+from transformers import BartForConditionalGeneration
 from transformers.models.bart.modeling_bart import shift_tokens_right
-from transformers.modeling_outputs import Seq2SeqLMOutput
+from transformers.modeling_outputs import Seq2SeqLMOutput, BaseModelOutput
+from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple, Union
+from inspect import signature
+from tqdm import tqdm
 
-class JoinModel(nn.Module):
-  def __init__(self, classifiers, classifier_tokenizer, seq2seq_model, seq2seq_tokenizer):
-    super(JoinModel, self).__init__()
+class BartForConditionalGenerationJoinModel(BartForConditionalGeneration):
+  def __init__(self, config, join_dropout=0.0, classifiers=None):
+    super(BartForConditionalGenerationJoinModel, self).__init__(config)
 
     # BART Seq2Seq Initializations
-    self.seq2seq_tokenizer = BartTokenizer.from_pretrained(seq2seq_tokenizer)
-    self.seq2seq_model = BartForConditionalGeneration.from_pretrained(seq2seq_model)
-    self.encoder = self.seq2seq_model.get_encoder()
-    self.decoder = self.seq2seq_model.get_decoder()
+    if torch.cuda.is_available():
+      self = self.cuda()
 
-    # BERT Classifier Initializations
-    self.classifier_tokenizer = BertTokenizer.from_pretrained(classifier_tokenizer)
     self.classifiers = []
-    for classifier in classifiers:
+    for classifier in tqdm(classifiers):
       model = BertForSequenceClassification.from_pretrained(classifier)
+      if torch.cuda.is_available():
+        model = model.cuda()
       self.classifiers.append(model)
     self.classification_hidden = self.classifiers[0].config.num_hidden_layers
     self.classification_heads = self.classifiers[0].config.num_attention_heads
 
     # Join Embedding
-    num_v = 4 * self.classification_heads
-    dim_v = self.seq2seq_model.config.d_model
+    num_v = len(self.classifiers)
+    dim_v = self.config.d_model
     self.v = nn.Parameter(torch.randn(num_v, dim_v), requires_grad=True)
+    self.join_dropout = nn.Dropout(p=join_dropout)
 
-    # Misc.
-    self.max_length = 128
+  #def prepare_inputs_for_generation(self, input_ids: torch.LongTensor, **kwargs) -> Dict[str, Any]:
+  def prepare_inputs_for_generation(self, input_ids, **kwargs):
+    """
+    Implement in subclasses of :class:`~transformers.PreTrainedModel` for custom behavior to prepare inputs in the
+    generate method.
+    """
+    params = signature(self.forward).parameters
+    param_dict = {'input_ids': input_ids}
+    for arg,val in kwargs.items():
+      if arg in params:
+        param_dict[arg] = val
+    return param_dict
 
-  def __inference_forward(self, post):
-    raise NotImplementedError
-
-  def forward(self, post, target=None):
-    if target is None:
-      return self.__inference_forward(post)
-
-    # Encode input
-    encoder_inputs = self.seq2seq_tokenizer(
-        post,
-        return_tensors='pt',
-        padding="max_length",
-        max_length=self.max_length
+  def _get_enrichment(self, classifier_inputs, classifier_attention, batch_size):
+    output = self.classifiers[0](
+        input_ids=classifier_inputs,
+        attention_mask=classifier_attention,
+        output_attentions=True
     )
-    labels = self.seq2seq_tokenizer(
-        target,
-        return_tensors='pt',
-        padding="max_length",
-        max_length=self.max_length
-    ).input_ids
-    encoder_outputs = self.encoder(**encoder_inputs)
-    encoder_hidden_size = encoder_outputs.last_hidden_state.shape[-1]
-    input_size = encoder_inputs.input_ids.shape[-1]
+    attn_layers = output.attentions[-1].mean(dim=2)
 
-    # Classify Input and get Attention
-    classifier_inputs = self.classifier_tokenizer(
-        post,
-        return_tensors='pt',
-        padding="max_length",
-        max_length=self.max_length)
-    attn_layers = torch.empty(4 * self.classification_heads, input_size)
-    for i,classifier in enumerate(self.classifiers):
-      start = i * self.classification_heads
+    for i,classifier in enumerate(self.classifiers[1:]):
+      start = (i + 1) * self.classification_heads
       end = start + self.classification_heads
 
-      output = classifier(**classifier_inputs, output_attentions=True)
-      attn_layers[start:end] = output.attentions[-1].mean(dim=2)
+      output = classifier(
+          input_ids=classifier_inputs,
+          attention_mask=classifier_attention,
+          output_attentions=True
+      )
 
-    enrichment = (attn_layers[:,:,None] * self.v[:,None,:]).sum(axis=0)
-    enriched_hidden_state = enrichment + encoder_outputs.last_hidden_state
-    print(enriched_hidden_state.shape)
+      attn_layers = torch.cat(
+          (attn_layers, output.attentions[-1].mean(dim=2)),
+          dim=1
+      )
 
-    decoder_input_ids = labels
-    decoder_outputs = self.decoder(
+    v_dropout = self.join_dropout(self.v)
+    attn_layers = torch.reshape(
+        attn_layers,
+        (batch_size, len(self.classifiers), self.classification_heads, -1)
+    )
+    return (attn_layers[:,:,:,:,None] * v_dropout[:,None,None,:]).sum(axis=(1,2))
+
+  def _add_enrichment_to_beam(self, encoder_outputs, enrichment, batch_size, curr_input_size):
+      encoder_hidden_size = encoder_outputs.last_hidden_state.shape[2]
+      input_size = encoder_outputs.last_hidden_state.shape[1]
+      num_beams = curr_input_size // batch_size
+
+      encoder_outputs.last_hidden_state = torch.reshape(
+          input=encoder_outputs.last_hidden_state,
+          shape=(batch_size, num_beams, input_size, encoder_hidden_size)
+      )
+      encoder_outputs.last_hidden_state[:,:input_size] = enrichment[:,None,:,:] + encoder_outputs.last_hidden_state[:,:input_size]
+      encoder_outputs.last_hidden_state = torch.reshape(
+          input=encoder_outputs.last_hidden_state,
+          shape=(-1, input_size, encoder_hidden_size)
+      )
+
+  def forward(
+      self,
+      input_ids,
+      classifier_inputs,
+      attention_mask=None,
+      classifier_attention=None,
+      decoder_input_ids=None,
+      decoder_attention_mask=None,
+      head_mask=None,
+      decoder_head_mask=None,
+      encoder_outputs=None,
+      past_key_values=None,
+      inputs_embeds=None,
+      decoder_inputs_embeds=None,
+      labels=None,
+      use_cache=None,
+      output_attentions=None,
+      output_hidden_states=None,
+      return_dict=None,
+      beam_search=False,
+  ):
+    # Housekeeping
+    output_attentions = (
+        output_attentions if output_attentions is not None else self.config.output_attentions
+    )
+    output_hidden_states = (
+        output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+    )
+    use_cache = use_cache if use_cache is not None else self.config.use_cache
+    return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+    # Encode input
+    if encoder_outputs is None:
+      encoder_outputs = self.get_encoder()(
+          input_ids=input_ids,
+          attention_mask=attention_mask,
+          head_mask=head_mask,
+          inputs_embeds=inputs_embeds,
+          output_attentions=output_attentions,
+          output_hidden_states=output_hidden_states,
+          return_dict=return_dict,
+      )
+    # If the user passed a tuple for encoder_outputs, we wrap it in a BaseModelOutput when return_dict=True
+    elif return_dict and not isinstance(encoder_outputs, BaseModelOutput):
+        encoder_outputs = BaseModelOutput(
+            last_hidden_state=encoder_outputs[0],
+            hidden_states=encoder_outputs[1] if len(encoder_outputs) > 1 else None,
+            attentions=encoder_outputs[2] if len(encoder_outputs) > 2 else None,
+        )
+
+    # Enrich Hidden State with Classifier Attention
+    batch_size = classifier_inputs.shape[0]
+    enrichment = self._get_enrichment(classifier_inputs, classifier_attention, batch_size)
+    if beam_search:
+      print('Input Ids: ', input_ids.shape)
+      curr_input_size = input_ids.shape[0]
+      self._add_enrichment_to_beam(encoder_outputs, enrichment, batch_size, curr_input_size)
+    else:
+      encoder_outputs.last_hidden_state += enrichment
+
+    # Decode Output
+    if decoder_input_ids is None:
+      if labels is not None:
+        decoder_input_ids = shift_tokens_right(
+            labels, self.config.pad_token_id, self.config.decoder_start_token_id
+        )
+      elif decoder_inputs_embeds is None:
+        decoder_input_ids = shift_tokens_right(
+            input_ids, self.config.pad_token_id, self.config.decoder_start_token_id
+        )
+      decoder_attention_mask = decoder_input_ids != self.config.pad_token_id
+    
+    decoder_outputs = self.get_decoder()(
         input_ids=decoder_input_ids,
-        encoder_hidden_states=enriched_hidden_state
+        attention_mask=decoder_attention_mask,
+        encoder_hidden_states=encoder_outputs.last_hidden_state,
+        encoder_attention_mask=attention_mask,
+        head_mask=decoder_head_mask,
+        encoder_head_mask=head_mask,
+        past_key_values=past_key_values,
+        inputs_embeds=decoder_inputs_embeds,
+        use_cache=use_cache,
+        output_attentions=output_attentions,
+        output_hidden_states=output_hidden_states,
+        return_dict=return_dict,
     )
 
-    lm_logits = self.seq2seq_model.lm_head(decoder_outputs[0]) + \
-                    self.seq2seq_model.final_logits_bias
+    lm_logits = self.lm_head(decoder_outputs.last_hidden_state) + \
+                    self.final_logits_bias
 
     masked_lm_loss = None
     if labels is not None:
         loss_fct = nn.CrossEntropyLoss()
         masked_lm_loss = loss_fct(
-            lm_logits.view(-1, self.seq2seq_model.config.vocab_size),
+            lm_logits.view(-1, self.config.vocab_size),
             labels.view(-1)
         )
+
+    if not return_dict:
+        output = (lm_logits,) + outputs[1:]
+        return ((masked_lm_loss,) + output) if masked_lm_loss is not None else output
 
     return Seq2SeqLMOutput(
         loss=masked_lm_loss,
---
+++ src/seq2seq_utils.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+import torch
+from transformers import BertTokenizer, BartTokenizer
+
+def clean_post(df):
+  df.post = df.post.str.replace(r'\bRT\b', ' ', regex=True)
+  df.post = df.post.str.replace('(@[^\s]*\s|\s?@[^\s]*$)', ' ', regex=True)
+  df.post = df.post.str.replace('https?://[^\s]*(\s|$)',' ',regex=True)
+  df.post = df.post.str.strip()
+
+def clean_target(df):
+    df.targetStereotype = df.targetStereotype.replace(np.nan, '', regex=True)
+    df = df.groupby(['HITId', 'post'], as_index=False).agg({'targetStereotype':set})
+    df.targetStereotype = df.targetStereotype.apply(';'.join)
+    df.targetStereotype = df.targetStereotype.str.replace(r'(^;|;$)', '', regex=True)
+    df.rename(columns={"targetStereotype":"target"}, inplace=True)
+    return df
+
+def get_tokenized_data(
+    datasets,
+    seq2seq_model_name,
+    classifier_model_name,
+    labels=False
+):
+  def process_labels(target_tokenized):
+    target_tokenized['labels'] = [
+        [(l if l != seq2seq_tok.pad_token_id else -100) for l in label]
+        for label in target_tokenized['input_ids']
+    ]
+    
+    del target_tokenized['input_ids']
+    del target_tokenized['attention_mask']
+  
+  def tokenize(examples):
+    seq2seq_tokenized = seq2seq_tok(
+        examples['post'],
+        padding="max_length",
+        truncation=True,
+        max_length=128,
+    )
+  
+    classifier_tokenized = classifier_tok(
+        examples['post'],
+        padding="max_length",
+        truncation=True,
+        max_length=128,
+    )
+  
+    seq2seq_tokenized['input_ids'] = seq2seq_tokenized['input_ids']
+    seq2seq_tokenized['attention_mask'] = seq2seq_tokenized['attention_mask']
+    
+    classifier_tokenized['classifier_inputs'] = classifier_tokenized['input_ids']
+    classifier_tokenized['classifier_attention'] = classifier_tokenized['attention_mask']
+
+    if labels:
+      with seq2seq_tok.as_target_tokenizer():
+        target_tokenized = seq2seq_tok(
+          examples['target'],
+          padding="max_length",
+          truncation=True,
+          max_length=128,
+        )
+      process_labels(target_tokenized)  
+      return {**seq2seq_tokenized, **classifier_tokenized, **target_tokenized}
+    return {**seq2seq_tokenized, **classifier_tokenized}
+  
+  #### get_tokenized_data function body
+  seq2seq_tok = BartTokenizer.from_pretrained(seq2seq_model_name)
+  classifier_tok = BertTokenizer.from_pretrained(classifier_model_name)
+ 
+  tokenized = datasets.map(
+      tokenize, batched=True,
+      num_proc=4,
+      remove_columns=['post','target','HITId']
+  )
+
+  return seq2seq_tok, classifier_tok, tokenized
+
